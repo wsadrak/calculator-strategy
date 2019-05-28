9bd95bfe5433c9c5fa919aeee5a19590ede729b9
wsadrak <w.sadrak@gmail.com>
2019-05-28 13:54:39 +0200

    refactoring: divide methods to smaller parts, rebuild of main loop

--- src/app/CalculatorController.java
+++ src/app/CalculatorController.java
@@ -5,46 +5,44 @@ import io.DataReader;
 import model.Calculator;
 
 public class CalculatorController {
-	
+	Calculator calculator = new Calculator();
 	DataReader dataReader = new DataReader();
-	public void mainLoop() {
-		double a = 0;
-		double b = 0;
-		String operator = null; 
-		boolean readComplete = false;
-		
-		// 1. wczytywanie wartoœci
-		do {
 
+	private double firstValue;
+	private double secondValue;
+	private String operator;
+	private double result;
+	boolean calculationsComplete;
+
+	public void mainLoop() {
+
+		while (!calculationsComplete) {
 			try {
-				a = dataReader.readDoubleFromUser();
-				b = dataReader.readDoubleFromUser();
-				operator = dataReader.readCharacter();
-				readComplete = true;
-			} catch (NumberFormatException e) {
-				System.out.println("Wprowadzono nieprawid³owe znaki. Spróbuj jeszcze raz.");
+				readValues();
+				calculateResult();
+
+			} catch (NumberFormatException | UnknownOperatorException e) {
+				System.out.println("Nieprawid³owy format danych. Spróbuj ponownie.");
 			}
 
-		} while (!readComplete);
-
-		// 2. obliczanie wyniku
-		Calculator calc = new Calculator();
-		double result = 0;
-		boolean calculatonComplete = false;
-		try {
-			result = calc.doOperation(a, b, operator);
-			calculatonComplete = true;
-		} catch (UnknownOperatorException | ArithmeticException e) {
-			System.err.println(e.getMessage());
-		}
-		
-		// 3. wyœwietlanie wyniku
-		if(calculatonComplete) {
-			System.out.println(a + operator + b + "=" + result);
-		} else {
-			System.out.println("Nie mo¿na by³o obliczyæ wyniku wyra¿enia " + a + operator + b);
 		}
 	}
 
+	private void printingResult() {
+		System.out.println(firstValue + operator + secondValue + "=" + result);
+	}
+
+	private void calculateResult() {
+		result = calculator.doOperation(firstValue, secondValue, operator);
+		calculationsComplete = true;
+		if (calculationsComplete)
+			printingResult();
+	}
+
+	private void readValues() {
+		firstValue = dataReader.readDoubleFromUser();
+		secondValue = dataReader.readDoubleFromUser();
+		operator = dataReader.readCharacter();
+	}
 
 }
