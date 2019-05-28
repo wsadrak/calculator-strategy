1db577bc4fd64485a0e843fbd473bd4bad77c8d5
wsadrak <w.sadrak@gmail.com>
2019-05-28 13:15:41 +0200

    big refactoring - dividing class to smaller classes

--- src/app/CalculatorController.java
+++ src/app/CalculatorController.java
@@ -1,85 +1,50 @@
 package app;
-import java.util.Scanner;
+
+import exception.UnknownOperatorException;
+import io.DataReader;
+import model.Calculator;
 
 public class CalculatorController {
 	
-	Scanner input = new Scanner(System.in);
-
+	DataReader dataReader = new DataReader();
 	public void mainLoop() {
-
-		boolean correctValues = false;
+		double a = 0;
+		double b = 0;
+		String operator = null; 
+		boolean readComplete = false;
+		
+		// 1. wczytywanie wartoœci
 		do {
 
 			try {
-				int a = readIntFromUser();
-				String sign = readCharacter();
-				int b = readIntFromUser();
-				doOperation(a, b, sign);
-				correctValues = true;
-			} catch (IllegalArgumentException e) {
+				a = dataReader.readDoubleFromUser();
+				b = dataReader.readDoubleFromUser();
+				operator = dataReader.readCharacter();
+				readComplete = true;
+			} catch (NumberFormatException e) {
 				System.out.println("Wprowadzono nieprawid³owe znaki. Spróbuj jeszcze raz.");
 			}
 
-		} while (!correctValues);
+		} while (!readComplete);
 
-	}
-
-	private void doOperation(int a, int b, String sign) throws IllegalArgumentException {
-		switch (sign) {
-		case "+":
-			add(a, b);
-			break;
-		case "-":
-			subtract(a, b);
-			break;
-		case "*":
-			multiply(a, b);
-			break;
-		case "/":
-			divide(a, b);
-			break;
-		default:
-			throw new IllegalArgumentException();
+		// 2. obliczanie wyniku
+		Calculator calc = new Calculator();
+		double result = 0;
+		boolean calculatonComplete = false;
+		try {
+			result = calc.doOperation(a, b, operator);
+			calculatonComplete = true;
+		} catch (UnknownOperatorException | ArithmeticException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		// 3. wyœwietlanie wyniku
+		if(calculatonComplete) {
+			System.out.println(a + operator + b + "=" + result);
+		} else {
+			System.out.println("Nie mo¿na by³o obliczyæ wyniku wyra¿enia " + a + operator + b);
 		}
 	}
 
-	private void divide(int a, int b) {
-		System.out.println("Dodawanie");
-		Context context = new Context(new OperationDivide());
-		System.out.println(a + " + " + b + " = " + context.executeStrategy(a, b));
-		
-	}
-
-	private void multiply(int a, int b) {
-		System.out.println("Dodawanie");
-		Context context = new Context(new OperationMultiply());
-		System.out.println(a + " + " + b + " = " + context.executeStrategy(a, b));
-		
-	}
-
-	private void subtract(int a, int b) {
-		System.out.println("Dodawanie");
-		Context context = new Context(new OperationSubtract());
-		System.out.println(a + " + " + b + " = " + context.executeStrategy(a, b));
-		
-	}
-
-	private String readCharacter() {
-		System.out.println("Podaj znak: ");
-		String sign = input.nextLine();
-		return sign;
-	}
-
-	private int readIntFromUser() {
-		System.out.println("Podaj pierwsz¹ liczbê: ");
-		int a = Integer.parseInt(input.nextLine());
-		return a;
-	}
-
-	private void add(int a, int b) {
-		System.out.println("Dodawanie");
-		Context context = new Context(new OperationAdd());
-		System.out.println(a + " + " + b + " = " + context.executeStrategy(a, b));
-	}
 
 }
