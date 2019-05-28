f9265ce96cd2e04b92ea2568f398393a6ed04fb8
wsadrak <w.sadrak@gmail.com>
2019-05-28 14:41:05 +0200

    removing a method for reading double values - conversion in another class

--- src/io/DataReader.java
+++ src/io/DataReader.java
@@ -5,15 +5,7 @@ import java.util.Scanner;
 public class DataReader {
 	private Scanner input = new Scanner(System.in);
 
-	public double readDoubleFromUser() {
-		System.out.println("Please type a number: ");
-		double number = Double.parseDouble(input.nextLine());
-		return number;
-	}
-
-	public String readCharacter() {
-		System.out.println("Please type an operator: ");
-		String sign = input.nextLine();
-		return sign;
+	public String readInputFromUser() {
+		return input.nextLine();
 	}
 }
